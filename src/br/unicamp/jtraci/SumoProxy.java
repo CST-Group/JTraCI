322f4f359a69a45a5ba151ba31c045bef736a76e
andre-paraense-mobwise <andre.paraense@mobwise.com.br>
2016-09-21 18:51:44 -0300

    - initial structure,description, license

---
+++ src/br/unicamp/jtraci/SumoProxy.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2016  DCA-FEEC-UNICAMP
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v3
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ * 
+ * Contributors:
+ *     A. L. O. Paraense, E. Froes, R. R. Gudwin
+ ******************************************************************************/
+
+package br.unicamp.jtraci;
+
+import java.net.Socket;
+
+/**
+ * @author andre.paraense
+ *
+ */
+public class SumoProxy {
+	
+	private Socket socket;
+	
+
+}
